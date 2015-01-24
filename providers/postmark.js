e9ed5d23095da5e2b5f6e8e7b6299a1a79292ae7
Josh Cole <saikojosh@gmail.com>
2015-01-24 17:06:35 +0000

    Started work on postmark provider

---
+++ providers/postmark.js
@@ -0,0 +1,37 @@
+/*
+ * Postmark provider.
+ */
+
+var ME       = module.exports;
+var postmark = require('postmark');
+
+/*
+ * Initialise the provider.
+ */
+ME.init = function (options) {
+
+
+
+};
+
+/*
+ * Prepare attachments in the format required by the provider.
+ * callback(err, attachments);
+ */
+ME.prepareAttachments = function (attachments, callback) {
+
+  
+
+
+
+};
+
+/*
+ * Sends the email via the provider.
+ * callback(err, success);
+ */
+ME.send = function (email, callback) {
+
+
+
+};
