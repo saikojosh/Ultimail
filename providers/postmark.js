a28597e3d52496b2c9cd85b5c97215b09e91884b
Josh Cole <saikojosh@gmail.com>
2015-01-25 17:34:05 +0000

    Continued work on Ultimail

--- providers/postmark.js
+++ providers/postmark.js
@@ -2,27 +2,22 @@
  * Postmark provider.
  */
 
-var ME       = module.exports;
-var postmark = require('postmark');
+var ME           = module.exports;
+var Postmark     = require('postmark');
+var objectAssign = require('object-assign');
+var _            = require('underscore');
 
 /*
- * Initialise the provider.
+ * Constructor.
  */
-ME.init = function (options) {
+function PostmarkProvider (options) {
 
+  options = objectAssign({
+    apiKey: null
+  }, options);
 
-
-};
-
-/*
- * Prepare attachments in the format required by the provider.
- * callback(err, attachments);
- */
-ME.prepareAttachments = function (attachments, callback) {
-
-  
-
-
+  // Initialise Postmark with the key.
+  this.postmark = new Postmark(options.apiKey);
 
 };
 
@@ -30,8 +25,62 @@ ME.prepareAttachments = function (attachments, callback) {
  * Sends the email via the provider.
  * callback(err, success);
  */
-ME.send = function (email, callback) {
+PostmarkProvider.prototype.send = function (email, callback) {
 
+  var postmark = this.postmark;
 
+  // First convert the attachments to the correct format.
+  this.prepareAttachments(email.attachments, function (err, attachments) {
+
+    if (err) { return callback(err); }
+
+    // Send the email.
+    postmark.send({
+      "To":          email.to.join(','),
+      "Cc":          email.cc.join(','),
+      "Bcc":         email.bcc.join(','),
+      "From":        email.from,
+      "ReplyTo":     email.replyTo,
+      "Subject":     email.subject,
+      "HtmlBody":    email.htmlBody,
+      "TextBody":    email.textBody,
+      "Attachments": attachments
+    }, function (err, success) {
+      if (err) { return callback(err); }
+      return callback(null, success);
+    });
+
+  });
 
 };
+
+/*
+ * Prepare attachments in the format required by the provider.
+ * callback(err, attachments);
+ */
+PostmarkProvider.prototype.prepareAttachments = function (input, callback) {
+
+  var attachments = [];
+
+  // No attachments to attach.
+  if (!input || !_.isArray(input) || input.length === 0) {
+    return callback(null, attachments);
+  }
+
+  // Convert attachments array to Postmark format.
+  for (var i = 0, ilen = input.length ; i < ilen ; i++) {
+    attachments.push({
+      "Name":        input[i].filename,
+      "ContentType": input[i].mimeType,
+      "Content":     input[i].data
+    });
+  }
+
+  return callback(null, attachments);
+
+};
+
+/*
+ * Export the constructor.
+ */
+module.exports = PostmarkProvider;
--- ultimail.js
+++ ultimail.js
@@ -5,6 +5,7 @@
 var fs           = require('fs');
 var async        = require('async');
 var juice2       = require('juice2');
+var handlebars   = require('handlebars');
 var marked       = require('marked');
 var objectAssign = require('object-assign');
 var _            = require('underscore');
@@ -16,40 +17,81 @@ function Ultimail (options) {
 
   // Default options.
   options = objectAssign({
-    provider:   'postmark',
-    viewEngine: 'handlebars',
-    markdown:   false
+    provider:  null,
+    markdown:  false,
+    styles:    true,
+    variables: null
   }, options);
 
   // Save the options.
   this.options = options;
 
   // Load the default provider and view engine.
-  this.provider   = this.loadProvider(options.provider);
-  this.viewEngine = this.loadViewEngine(options.viewEngine);
+  this.provider = this.loadProvider(options.provider);
 
 };
 
 /*
  * Sends an email. Default options can be overridden.
  * callback(err, success);
+ * [Usage]
+ *  [1] send(tpl, options, callback);
+ *  [2] send(email, options, callback);
  */
-Ultimail.prototype.send = function (tpl, options, callback) {
+Ultimail.prototype.send = function (input, options, callback) {
 
-  var that = this;
+  options = objectAssign({
+    provider: null
+  }, options);
 
-  // First prepare the email.
-  this.prepare(tpl, options, function (err, email) {
+  var provider;
+
+  // Are we overriding the provider?
+  if (options.provider && options.provider.name !== this.provider.name) {
+    provider = this.loadProvider(options.provider);
+  } else {
+    provider = this.provider;
+  }
+
+  // An email has been passed in.
+  if (this.isEmail(input)) { return provider.send(input, callback); }
+
+  // Otherwise prepare the email first.
+  this.prepare(input, options, function (err, email) {
 
     if (err) { return callback(err); }
 
     // Send the email via the provider.
-    that.provider.send(email, function (err, success) {
-      if (err) { return callback(err); }
-      return callback(null, success);
-    });
+    return provider.send(email, callback);
 
   });
+
+};
+
+/*
+ * Sends a pre-prepared email using the provider. Doesn't do any pre-processing
+ * on the email.
+ */
+Ultimail.prototype.quickSend = function (options, callback) {
+
+  options = objectAssign({
+    to:          null,
+    cc:          null,
+    bcc:         null,
+    from:        null,
+    replyTo:     null,
+    subject:     null,
+    htmlBody:    null,
+    textBody:    null,
+    attachments: null,
+    provider:    null
+  }, options);
+
+  // Setup email.
+  var email = new this.Email(options);
+
+  // Send the email!
+  return this.send(email, options, callback);
 
 };
 
@@ -61,95 +103,33 @@ Ultimail.prototype.prepare = function (tpl, options, callback) {
 
   options = objectAssign({
     to:          null,
+    cc:          null,
+    bcc:         null,
     from:        null,
     replyTo:     null,
     subject:     null,
-    variables:   null,
     attachments: null,
-    provider:    null,
-    viewEngine:  null,
-    markdown:    null
+    variables:   null,
+    markdown:    null,
+    styles:      null
   }, options);
 
-  var that        = this;
   var tplHtmlBody = tpl + '/body.html';
   var tplTextBody = tpl + '/body.txt';
   var tplSubject  = tpl + '/subject.txt';
-  var email = {
-    subject:     '',
-    htmlBody:    '',
-    textBody:    '',
-    attachments: []
-  };
+  var markdown    = objectAssign({}, this.markdown, options.markdown);
+  var styles      = (options.styles !== null ? options.styles : this.styles);
 
-  // Are we overriding these options or using the defaults?
-  var provider    = (options.provider   !== null ? this.loadProvider(options.provider)     : this.provider);
-  var viewEngine  = (options.viewEngine !== null ? this.loadViewEngine(options.viewEngine) : this.viewEngine);
-  var markdown    = (options.markdown   !== null ? options.markdown                        : this.markdown);
+  // Setup email.
+  var email = new this.Email(options);
 
-  // Ensure correct data types.
-  if (!_.isArray(options.to)) { options.to = [options.to]; }
-
-  // Prepare the email in stages.
+  // Start preparing the email.
   async.waterfall([
 
-    // Load the HTML body template.
-    function loadHtmlBody (next) {
+    // Load the HTML body and process the template.
+    function prepareHtmlBody (next) {
 
-      juice2(tplHtmlBody, function(err, output) {
-
-        if (err) { return next(err); }
-
-        // Save and continue.
-        email.bodyHtml = output;
-        return next(null);
-
-      });
-
-    },
-
-    // Load the text body template.
-    function loadTextBody (next) {
-
-      fs.readFile(tplTextBody, function (err, data) {
-
-        if (err) { return next(err); }
-
-        // Save and continue.
-        email.textBody = data;
-        return next(null);
-
-      });
-
-    },
-
-    // Load the subject template.
-    function loadSubject (next) {
-
-      // If we're overriding the subject don't bother loading the template file.
-      if (options.subject) {
-        email.subject = options.subject;
-        return next(null);
-      }
-
-      // Load the subject template.
-      fs.readFile(tplSubject, function (err, data) {
-
-        if (err) { return next(err); }
-
-        // Save and continue.
-        email.subject = data;
-        return next(null);
-
-      });
-
-    },
-
-    // Process the HTML body template.
-    function processHtmlBody (next) {
-
-      // Parse the view engine.
-      that.viewEngine.process(email.htmlBody, function (err, output) {
+      ME.prepareTemplate(tplHtmlBody, options.variables, function (err, output) {
 
         if (err) { return next(err); }
 
@@ -161,33 +141,177 @@ Ultimail.prototype.prepare = function (tpl, options, callback) {
 
     },
 
-    // Process markdown.
-    function processMarkdown (next) {
+    // Load the text body and process the template.
+    function prepareTextBody (next) {
 
-      // Setup markdown parser if options have been given.
-      if (_.isObject(that.markdown)) { marked.setOptions(that.markdown); }
+      ME.prepareTemplate(tplTextBody, options.variables, function (err, output) {
 
-      // Parse markdown.
-      marked(input, function (err, output) {
         if (err) { return next(err); }
+
+        // Save and continue.
+        email.textBody = output;
         return next(null);
+
       });
 
     },
 
-    function (next) {
+    // Load the subject and process the template.
+    function prepareSubject (next) {
 
+      // Don't bother loading the template if we are overriding the subject.
+      if (options.subject) {
+        email.subject = options.subject;
+        return next(null);
+      }
 
+      ME.prepareTemplate(tplSubject, options.variables, function (err, output) {
+
+        if (err) { return next(err); }
+
+        // Save and continue.
+        email.subject = output;
+        return next(null);
+
+      });
 
     },
 
-    function (next) {
+    // Process markdown on the HTML body.
+    function markdownHtmlBody (next) {
+
+      // Skip if no HTML body.
+      if (!email.htmlBody) { return next(null); }
+
+      // Skip if we aren't processing markdown.
+      if (!markdown) { return next(null); }
+
+      // Default options (as per "marked" module).
+      var defaultOptions = {
+        gfm:         true,
+        tables:      true,
+        breaks:      false,
+        pedantic:    false,
+        sanitize:    true,
+        smartLists:  true,
+        smartypants: false
+      };
+
+      // Merge default options with global and instance options.
+      var finalOptions = objectAssign({}, defaultOptions, markdown);
+
+      // Set markdown options.
+      marked.setOptions(finalOptions);
+
+      // Parse markdown.
+      marked(email.htmlBody, function (err, output) {
+
+        if (err) { return next(err); }
+
+        // Save and continue.
+        email.htmlBody = output;
+        return next(null);
+
+      });
+
+    },
+
+    // Process styles on the HTML body.
+    function juiceHtmlBody (next) {
+
+      // Skip if no HTML body.
+      if (!email.htmlBody) { return next(null); }
+
+      // Styles have been disabled.
+      if (!styles) { return next(null); }
+
+      // Inline the CSS styles.
+      juice2.juiceContent(email.bodyHtml, {
+        url: 'file://' + tpl
+      }, function(err, output) {
+
+        if (err) { return next(err); }
+
+        // Save and continue.
+        email.bodyHtml = output;
+        return next(null);
+
+      });
 
     }
 
   ], function (err) {
     if (err) { return callback(err); }
-    return callback(null, email);
+    return callback(err, email);
+  });
+
+};
+
+/*
+ * Returns a new email object.
+ * [Usage]
+ *  new this.Email({ ... });
+ */
+Ultimail.prototype.Email = function (options) {
+
+  // Default to Null if no values are set.
+  var to  = options.to  || null;
+  var cc  = options.cc  || null;
+  var bcc = options.bcc || null;
+
+  // Typecast values to arrays.
+  if (to  !== null) { to  = (_.isArray(to)  ? to  : [to]);  }
+  if (cc  !== null) { cc  = (_.isArray(cc)  ? cc  : [cc]);  }
+  if (bcc !== null) { bcc = (_.isArray(bcc) ? bcc : [bcc]); }
+
+  // Create the email.
+  var email = {
+    to:          to,
+    cc:          cc,
+    bcc:         bcc,
+    from:        options.from || null,
+    replyTo:     options.replyTo || null,
+    htmlBody:    '',
+    textBody:    '',
+    subject:     '',
+    attachments: options.attachments || []
+  };
+
+  // Add a method for sending email later.
+  // callback(err, success);
+  var instance = this;
+  email.send = function (options, callback) {
+    instance.send(email, options, callback);
+  };
+
+  return email;
+
+};
+
+/*
+ * Load and parse the template.
+ * callback(err, output);
+ */
+Ultimail.prototype.prepareTemplate = function (tplName, variables, callback) {
+
+  // Load the template.
+  fs.readFile(tplName, function (err, data) {
+
+    // No template.
+    if (err || !data) { return callback(null); }
+
+    // Compile the handlebars template.
+    try {
+      var hbTpl  = handlebars.compile(data);
+      var output = hbTpl(variables);
+    }
+    catch (err) {
+      return callback(err);
+    }
+
+    // Success!
+    return callback(null, output);
+
   });
 
 };
@@ -210,15 +334,24 @@ Ultimail.prototype.loadProvider = function (options) {
 /*
  * Returns the given view engine.
  */
-Ultimail.prototype.loadViewEngine = function (options) {
+Ultimail.prototype.isEmail = function (input) {
 
-  var name       = (_.isObject(options) ? options.name : options);
-  var viewEngine = (name ? require('viewEngines/' + name + '.js') : null);
+  // Must be a hash.
+  if (!_.isObject(input))               { return false; }
 
-  // Setup the view engine.
-  if (viewEngine) { viewEngine.init(options); }
+  // Check properties.
+  if (_.isUndefined(input.to))          { return false; }
+  if (_.isUndefined(input.cc))          { return false; }
+  if (_.isUndefined(input.bcc))         { return false; }
+  if (_.isUndefined(input.from))        { return false; }
+  if (_.isUndefined(input.replyTo))     { return false; }
+  if (_.isUndefined(input.htmlBody))    { return false; }
+  if (_.isUndefined(input.textBody))    { return false; }
+  if (_.isUndefined(input.subject))     { return false; }
+  if (_.isUndefined(input.attachments)) { return false; }
 
-  return viewEngine;
+  // Success! The input is an email.
+  return true;
 
 };
 
