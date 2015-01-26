3e2ee0759860cd852adfb720f43e73e19d9b3e77
Josh Cole <saikojosh@gmail.com>
2015-01-26 10:18:50 +0000

    Removed unnecessary styles property from examples

--- examples/send.js
+++ examples/send.js
@@ -10,7 +10,6 @@ var mailer = new Ultimail({
     name:   'postmark',
     apiKey: '0a1d6715-33f7-4dda-b3a2-892115138935'
   },
-  styles: true,
   variables: {
     brandName: 'Amazing Widgets Ltd',
     website:   'http://www.aw-ltd.co.uk'
