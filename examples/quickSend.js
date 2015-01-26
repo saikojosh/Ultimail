0980b8e1ddea251a77e8c0e1b6d9833a57910aaa
Josh Cole <saikojosh@gmail.com>
2015-01-26 10:16:20 +0000

    Added - quickSend() example

--- examples/quickSend.js
+++ examples/quickSend.js
@@ -1,4 +1,32 @@
 /*
- * Module description.
+ * mailer.quickSend() example.
  */
 
+var Ultimail = require('../ultimail');
+
+// Create a new mailer.
+var mailer = new Ultimail({
+  provider: {
+    name:   'postmark',
+    apiKey: '0a1d6715-33f7-4dda-b3a2-892115138935'
+  },
+  markdown:  false,
+  styles:    true,
+  variables: {
+    brandName: 'Amazing Widgets Ltd',
+    website:   'http://www.aw-ltd.co.uk'
+  }
+});
+
+// Send an email immediately without any pre-processing.
+mailer.quickSend({
+  to:       'user-email@aw-ltd.co.uk',
+  from:     'webmaster@aw-ltd.co.uk',
+  subject:  'My Subject: {{firstName}}',
+  htmlBody: '<h1>Hello,  {{firstName}}!</h1>'
+}, function (err, success) {
+
+  console.log('err', err);
+  console.log('success', success);
+
+});
