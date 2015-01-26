16e9337f8331e34f00ba835cecfa4910ad13f478
Josh Cole <saikojosh@gmail.com>
2015-01-26 10:16:14 +0000

    Added - send() example

--- examples/send.js
+++ examples/send.js
@@ -1,4 +1,34 @@
 /*
- * Module description.
+ * mailer.send() example.
  */
 
+var Ultimail = require('../ultimail');
+
+// Create a new mailer.
+var mailer = new Ultimail({
+  provider: {
+    name:   'postmark',
+    apiKey: '0a1d6715-33f7-4dda-b3a2-892115138935'
+  },
+  markdown:  false,
+  styles:    true,
+  variables: {
+    brandName: 'Amazing Widgets Ltd',
+    website:   'http://www.aw-ltd.co.uk'
+  }
+});
+
+// Prepare and send an email immediately.
+mailer.send(__dirname + '/emailTemplates/welcome', {
+  to:   'user-email@aw-ltd.co.uk',
+  from: 'webmaster@aw-ltd.co.uk',
+  variables: {
+    firstName: 'Josh',
+    lastName:  'Cole'
+  }
+}, function (err, success) {
+
+  console.log('err', err);
+  console.log('success', success);
+
+});
