11ee3abc39c34574b174d0a8621e3b7b432a69bc
Josh Cole <saikojosh@users.noreply.github.com>
2017-06-26 10:35:29 +0100

    Fix - checking if a provider is configured

--- ultimail.js
+++ ultimail.js
@@ -317,7 +317,7 @@ module.exports = class Ultimail extends MiddlewareEngine {
 			throw new Error(`Either a "htmlBody" or a "plainBody" needs to be specified.`);
 		}
 
-		if (this.isConfigured(`provider`)) {
+		if (!this.isConfigured(`provider`)) {
 			throw new Error(`You must configure a "provider" handler to send emails.`);
 		}
 
